94f5fb880458989ec496260025b300ddb3a6658a
Paula Fahmy <paula@nacitadrive.com>
2023-02-24 17:50:46 +0200

    Add project files.

---
+++ ConnectFour/Program.cs
@@ -0,0 +1,27 @@
+﻿//Console.WriteLine("Please provide the size of the game, X:");
+var gameSizeX = 5; //int.Parse(Console.ReadLine());
+//Console.WriteLine("Please ddfgds the size of the game, Y:");
+var gameSizeY = 5; //int.Parse(Console.ReadLine());
+
+const int PLAYER_1_ID = 1;
+const int PLAYER_2_ID = 2;
+
+var column = new Stack<int>();
+var columns = new List<Stack<int>>();
+columns.AddRange(Enumerable.Repeat(column, gameSizeY));
+
+PrintGame();
+
+void PrintGame()
+{
+    for(int y = gameSizeY-1; y >= 0; y--)
+    {
+        for (int x = 0; x != gameSizeX; x++)
+        {
+            //Console.Write('_');
+            Console.Write($"({x}, {y})");
+            Console.Write(' ');
+        }
+        Console.WriteLine();
+    }
+}
