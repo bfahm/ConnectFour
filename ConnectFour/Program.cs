5d2ea3c2797a0f123432f3fe37fc35745ce0be58
Paula Fahmy <paula@nacitadrive.com>
2023-02-24 20:50:24 +0200

    Enhanced win helper

--- ConnectFour/Program.cs
+++ ConnectFour/Program.cs
@@ -37,22 +37,7 @@ GamePrinter.Print(game, onlyCoordinates: true);
 Console.WriteLine();
 Console.WriteLine();
 
-var candidate1 = game.GetDiagonalArround(new Point(1, 1));
-var candidate1IsWin = WinHelper.DetermineIsWinner(candidate1, '1');
-Console.WriteLine(candidate1);
-Console.WriteLine(candidate1IsWin);
-var candidate2 = game.GetReverseDiagonalArround(new Point(1, 1));
-var candidate2IsWin = WinHelper.DetermineIsWinner(candidate2, '1');
-Console.WriteLine(candidate2);
-Console.WriteLine(candidate2IsWin);
-var candidate3 = game.GetColumnArround(new Point(1, 1));
-var candidate3IsWin = WinHelper.DetermineIsWinner(candidate3, '1');
-Console.WriteLine(candidate3);
-Console.WriteLine(candidate3IsWin);
-var candidate4 = game.GetRowArround(new Point(1, 1));
-var candidate4IsWin = WinHelper.DetermineIsWinner(candidate4, '1');
-Console.WriteLine(candidate4);
-Console.WriteLine(candidate4IsWin);
+WinHelper.DetermineWinningState(game, new Point(1, 1), '1');
 
 Console.WriteLine();
 Console.WriteLine();
--- ConnectFour/WinHelper.cs
+++ ConnectFour/WinHelper.cs
@@ -2,10 +2,42 @@
 {
     internal class WinHelper
     {
-        public static bool DetermineIsWinner(string candidate, char playerIdString, int connectCount = 4)
+        private static bool DetermineIsWinner(string candidate, char playerIdString, int connectCount = 4)
         {
             string winString = new string(playerIdString, connectCount);
             return candidate.Contains(winString);
+        }
+
+        public static bool DetermineWinningState(Game game, Point playedPoint, char playerId)
+        {
+            Console.Write($"Player {playerId} \t");
+
+            var diagonalArroundPointCandidate = game.GetDiagonalArround(playedPoint);
+            var diagonalArroundPointCandidateIsWin = DetermineIsWinner(diagonalArroundPointCandidate, playerId);
+            //Console.WriteLine(diagonalArroundPointCandidate);
+            Console.Write($"Diagonal Win: {diagonalArroundPointCandidateIsWin} \t");
+
+            var reverseDiagonalArroundPointCandidate = game.GetReverseDiagonalArround(playedPoint);
+            var reverseDiagonalArroundCandidateIsWin = DetermineIsWinner(reverseDiagonalArroundPointCandidate, playerId);
+            //Console.WriteLine(reverseDiagonalArroundPointCandidate);
+            Console.Write($"Reverse Win: {reverseDiagonalArroundCandidateIsWin} \t");
+            
+            var columnArroundCandidate = game.GetColumnArround(playedPoint);
+            var columnArroundCandidateIsWin = DetermineIsWinner(columnArroundCandidate, playerId);
+            //Console.WriteLine(columnArroundCandidate);
+            Console.Write($"Column Win: {columnArroundCandidateIsWin} \t");
+            
+            var rowArroundPointCandidate = game.GetRowArround(playedPoint);
+            var rowArroundPointCandidateIsWin = DetermineIsWinner(rowArroundPointCandidate, playerId);
+            //Console.WriteLine(rowArroundPointCandidate);
+            Console.Write($"Row Win: {rowArroundPointCandidateIsWin} \t");
+
+            Console.WriteLine();
+
+            return diagonalArroundPointCandidateIsWin
+                   || reverseDiagonalArroundCandidateIsWin
+                   || columnArroundCandidateIsWin
+                   || rowArroundPointCandidateIsWin;
         }
     }
 }
