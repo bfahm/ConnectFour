3f76364ce730c8443c0819eda292da50388dc1f6
Paula Fahmy <paula@nacitadrive.com>
2023-02-24 19:21:55 +0200

    Added print diagonal funtion

--- ConnectFour/Game.cs
+++ ConnectFour/Game.cs
@@ -20,12 +20,62 @@
             State = columns;
         }
 
+        public void PrintDiagonalArround(int originX, int originY)
+        {
+            const int arround = 2;
+
+            //Forward Iteration
+            for (int i = 0; i <= arround; i++)
+            {
+                var accessX = originX + i;
+                var accessY = originY + i;
+
+                if(IsValidX(accessX) && IsValidY(accessY))
+                {
+                    Console.Write(GetItemAtCoordinates(accessX, accessY));
+                    Console.Write("-");
+                }
+            }
+            Console.WriteLine();
+
+            //Backward Iteration
+            for (int i = arround; i >= 0; i--)
+            {
+                var accessX = originX - i;
+                var accessY = originY - i;
+
+                if (IsValidX(accessX) && IsValidY(accessY))
+                {
+                    Console.Write(GetItemAtCoordinates(accessX, accessY));
+                    Console.Write("-");
+                }
+            }
+        }
+
         public int? GetItemAtCoordinates(int x, int y)
         {
             var currentColumn = State?[x];
             var currentItem = currentColumn?.Count() > y ? currentColumn?[y] : null;
 
             return currentItem;
+        }
+
+        private bool IsValidX(int x) => IsValidCoordinate(x, SizeX);
+        private bool IsValidY(int y) => IsValidCoordinate(y, SizeY);
+
+        private bool IsValidCoordinate(int axisIdx, int axisSize)
+        {
+            if (axisIdx < 0)
+            {
+                return false;
+            }
+
+            if(axisIdx > axisSize - 1)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
--- ConnectFour/Program.cs
+++ ConnectFour/Program.cs
@@ -12,13 +12,35 @@ var game = new Game(gameSizeX, gameSizeY);
 var columns = game.State;
 
 
-columns[0].Add(PLAYER_1_ID);
+columns[0].Add(PLAYER_2_ID);
 columns[1].Add(PLAYER_2_ID);
 columns[0].Add(PLAYER_1_ID);
+columns[1].Add(PLAYER_1_ID);
+columns[2].Add(PLAYER_2_ID);
+columns[2].Add(PLAYER_2_ID);
+columns[2].Add(PLAYER_1_ID);
+columns[3].Add(PLAYER_2_ID);
+columns[3].Add(PLAYER_2_ID);
+columns[3].Add(PLAYER_2_ID);
+columns[3].Add(PLAYER_1_ID);
+columns[4].Add(PLAYER_2_ID);
+columns[4].Add(PLAYER_2_ID);
+columns[4].Add(PLAYER_2_ID);
+columns[4].Add(PLAYER_2_ID);
+columns[4].Add(PLAYER_2_ID);
 
 GamePrinter.Print(game, onlyCoordinates: false);
 Console.WriteLine();
 Console.WriteLine();
 GamePrinter.Print(game, onlyCoordinates: true);
 
+Console.WriteLine();
+Console.WriteLine();
 
+//game.PrintDiagonalArround(0, 0);
+game.PrintDiagonalArround(2, 2);
+//game.PrintDiagonalArround(3, 3);
+//game.PrintDiagonalArround(5, 5);
+
+Console.WriteLine();
+Console.WriteLine();
