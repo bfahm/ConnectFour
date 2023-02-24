ba6aaebe0a4a9b414a4d2b2e47938b5872ea739b
Paula Fahmy <paula@nacitadrive.com>
2023-02-24 18:26:50 +0200

    Exported game logic into a separate class

---
+++ ConnectFour/Game.cs
@@ -0,0 +1,23 @@
+﻿namespace ConnectFour
+{
+    internal class Game
+    {
+        public int SizeX { get; set; }
+        public int SizeY { get; set; }
+        public List<List<int>> State { get; set; }
+
+        public Game(int gameSizeX, int gameSizeY) 
+        {
+            var columns = new List<List<int>>();
+            for (int y = 0; y < gameSizeY; y++)
+            {
+                var column = new List<int>();
+                columns.Add(column);
+            }
+
+            SizeX = gameSizeX;
+            SizeY = gameSizeY;
+            State = columns;
+        }
+    }
+}
--- ConnectFour/Program.cs
+++ ConnectFour/Program.cs
@@ -1,4 +1,6 @@
 ﻿//Console.WriteLine("Please provide the size of the game, X:");
+using ConnectFour;
+
 var gameSizeX = 5; //int.Parse(Console.ReadLine());
 //Console.WriteLine("Please ddfgds the size of the game, Y:");
 var gameSizeY = 5; //int.Parse(Console.ReadLine());
@@ -6,25 +8,24 @@ var gameSizeY = 5; //int.Parse(Console.ReadLine());
 const int PLAYER_1_ID = 1;
 const int PLAYER_2_ID = 2;
 
-var columns = new List<List<int>>();
-for(int y = 0; y < gameSizeY; y++)
-{
-    var column = new List<int>();
-    columns.Add(column);
-}
+var game = new Game(gameSizeX, gameSizeY);
+var columns = game.State;
 
 
 columns[0].Add(PLAYER_1_ID);
 columns[1].Add(PLAYER_2_ID);
 columns[0].Add(PLAYER_1_ID);
 
-PrintGame(true);
+PrintGame(game, onlyCoordinates: false);
+Console.WriteLine();
+Console.WriteLine();
+PrintGame(game, onlyCoordinates: true);
 
-void PrintGame(bool onlyCoordinates = false)
+void PrintGame(Game game, bool onlyCoordinates = false)
 {
-    for(int y = gameSizeY-1; y >= 0; y--)
+    for(int y = game.SizeY - 1; y >= 0; y--)
     {
-        for (int x = 0; x != gameSizeX; x++)
+        for (int x = 0; x != game.SizeX; x++)
         {
             if (onlyCoordinates)
                 PrintCurrentCoordinate(x, y);
@@ -39,7 +40,7 @@ void PrintGame(bool onlyCoordinates = false)
 
 void PrintCurrentValue(int x, int y)
 {
-    var currentColumn = columns?[x];
+    var currentColumn = game.State?[x];
     var currentItem = currentColumn?.Count() > y ? currentColumn?[y] : null;
 
     if (currentItem != null)
